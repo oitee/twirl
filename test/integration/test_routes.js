782fc8047c995e6ff24728b92b97577a2582ffde
Otee <oitee.codes@gmail.com>
2021-11-01 20:37:21 +0530

    Integration test for signup

--- test/integration/test_routes.js
+++ test/integration/test_routes.js
@@ -2,6 +2,7 @@ import launch from "../../src/server.js";
 import assert from "assert";
 import express from "express";
 import fetch from "node-fetch";
+import cookie from "cookie";
 
 function freePort() {
   const testApp = express();
@@ -13,20 +14,35 @@ function freePort() {
 
 async function basicRouteTests(port) {
   let baseUrl = `http://localhost:${port}`;
+
+  // -----------------------------------------------------------------
+  // GET /
+  // -----------------------------------------------------------------
   let response = await fetch(baseUrl, {
     method: "GET",
     redirect: "manual",
   });
   assert(response.status === 302, "GET / status is 302");
+  assert(response.headers.get("set-cookie") == undefined);  
 
+  // -----------------------------------------------------------------
+  // GET /login
+  // -----------------------------------------------------------------
   let redirectResponse = await fetch(response.headers.get("location"), {
     method: "GET",
     redirect: "manual",
   });
   assert(redirectResponse.status === 200, "GET /login status is 200");
   let responseBody = await redirectResponse.text();
-  assert(responseBody.startsWith(`<!DOCTYPE html>`), "GET /login returns HTML");
+  assert(
+    responseBody.includes(`<title>Twirl | Log-in</title>`),
+    "GET /login returns the correct page"
+  );
+  assert(redirectResponse.headers.get("set-cookie") == undefined);  
 
+  // -----------------------------------------------------------------
+  // POST /login (with non-existing credentials)
+  // -----------------------------------------------------------------
   let parameters = new URLSearchParams();
   parameters.append("username", "user1");
   parameters.append("password", "123");
@@ -42,6 +58,86 @@ async function basicRouteTests(port) {
     (await loginResponse.text()).includes("Credentials Incorrect"),
     "POST /login body contains incorrect credentials message"
   );
+  assert(loginResponse.headers.get("set-cookie") == undefined);
+
+  // -----------------------------------------------------------------
+  // POST /signup
+  // -----------------------------------------------------------------
+  let signUpResponse = await fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    body: parameters,
+    redirect: "manual",
+  });
+  assert.equal(
+    signUpResponse.status,
+    302,
+    "POST /signup returns with status 302"
+  );
+  let stringCookie = signUpResponse.headers.get("set-cookie");
+  let cookieObj = cookie.parse(stringCookie);
+  let signUpResponseCookieHeader = `_twirl=${cookieObj["_twirl"]}`;
+  console.log(signUpResponse.headers.get("location"));
+  let responseRedirectHome = await fetch(
+    signUpResponse.headers.get("location"),
+    {
+      method: "GET",
+      headers: { Cookie: signUpResponseCookieHeader },
+      redirect: "manual",
+    }
+  );
+  assert(
+    (await responseRedirectHome.text()).includes("<title>Twirl | Home</title>"),
+    "GET /home (redirect); body should contain correct title"
+  );
+  assert.equal(
+    responseRedirectHome.status,
+    200,
+    "GET /home (redirect); status should be 200"
+  );
+  assert(responseRedirectHome.headers.get("set-cookie") == undefined);
+
+  // -----------------------------------------------------------------
+  // GET /login with already signed user
+  // -----------------------------------------------------------------
+
+  let responseLogIn = await fetch(`${baseUrl}/login`, {
+    method: "GET",
+    headers: { Cookie: signUpResponseCookieHeader },
+    redirect: "manual",
+  });
+  assert.equal(
+    responseLogIn.status,
+    302,
+    "GET /login (with already signed user); status should be 302"
+  );
+  assert(
+    responseLogIn.headers.get("location"),
+    `${baseUrl}/home`,
+    "GET /login (with already signed in User); redirect location: home page"
+  );
+  assert(responseLogIn.headers.get("set-cookie") == undefined);
+
+  // -----------------------------------------------------------------
+  // GET /signup with already signed user
+  // -----------------------------------------------------------------
+
+  let responseSignUp = await fetch(`${baseUrl}/signup`, {
+    method: "GET",
+    headers: { Cookie: signUpResponseCookieHeader },
+    redirect: "manual",
+  });
+  assert.equal(
+    responseSignUp.status,
+    302,
+    "GET /signup (with already signed user); status should be 302"
+  );
+  assert(
+    responseSignUp.headers.get("location"),
+    `${baseUrl}/home`,
+    "GET /signup (with already signed in User); redirect location: home page"
+  );
+  assert(responseSignUp.headers.get("set-cookie") == undefined);
+  
 }
 let port;
 let server;
