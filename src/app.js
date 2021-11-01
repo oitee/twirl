8cedf619c8dbcad0ec9ebc19e3ef659574c572ef
Otee <oitee.codes@gmail.com>
2021-11-01 10:44:44 +0530

    Basic sign-up flow skeleton

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.get("/", (request, response) => {
   return response.send("hey!");
 });
+
+app.get("/home", (reques, response) => {
+  return response.sendFile("home.html", { root: "public" });
+});
+
+app.get("/signup", (request, response) => {
+  return response.sendFile("signup.html", { root: "public" });
+});
+
+app.get("/login", (request, response) => {
+    return response.sendFile("login.html", { root: "public" });
+  });
 
 app.listen(4000);
